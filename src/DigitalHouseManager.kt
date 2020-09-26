e8d2e90e77ff150f0c66bcfa2e3ca12a85b7e9a5
Jenifer Soares <jey.souza11@gmail.com>
2020-09-26 14:37:29 -0300

    Criar Metodos: Adicionar e Excluir Professores

--- src/DigitalHouseManager.kt
+++ src/DigitalHouseManager.kt
@@ -1,12 +1,12 @@
 internal class DigitalHouseManager {
 
-    var listaDeAlunos: MutableList<Aluno> = mutableListOf<Aluno>()
-    var listaDeProfesores: MutableList<Professor> = mutableListOf<Professor>()
-    var listaDeCursos: MutableList<Curso> = mutableListOf<Curso>()
-    var listaDeMatriculas: MutableList<Matricula> = mutableListOf<Matricula>()
+    var listaDeAlunos: MutableList<Aluno> = mutableListOf()
+    var listaDeProfesores: MutableList<Professor> = mutableListOf()
+    var listaDeCursos: MutableList<Curso> = mutableListOf()
+    var listaDeMatriculas: MutableList<Matricula> = mutableListOf()
 
     private fun registrarCurso(nome: String, codigoCurso: Int, quantidadeMaximaDeAlunos: Int){
-        val newCurso: Curso = Curso()
+        val newCurso = Curso()
         newCurso.nomeDoCurso = nome
         newCurso.codigoCurso = codigoCurso
         newCurso.quantidadeMaximaAlunos = quantidadeMaximaDeAlunos
@@ -16,6 +16,22 @@ internal class DigitalHouseManager {
     private fun excluirCurso(codigoCurso: Int) {
         listaDeCursos.forEach {
             if(codigoCurso == it.codigoCurso) listaDeCursos.removeAt(codigoCurso)
+        }
+    }
+
+    private fun registrarProfessorAdjunto(nome: String, sobrenome: String, codigoProfessor: Int, quantidadeDeHoras: Int){
+        val newProfessorAdjunto = ProfessorAdjunto(nome, sobrenome, 0, codigoProfessor, quantidadeDeHoras )
+        listaDeProfesores.add(newProfessorAdjunto)
+    }
+
+    private fun registrarProfessorTitular(nome: String, sobrenome: String, codigoProfessor: Int, especialidade: String){
+        val newProfessorTitular = ProfessorTitular(nome, sobrenome, 0, codigoProfessor, especialidade)
+        listaDeProfesores.add(newProfessorTitular)
+    }
+
+    private fun excluirProfessor(codigoProfessor: Int){
+        listaDeProfesores.forEach {
+            if(codigoProfessor == it.codigoProfessor) listaDeProfesores.removeAt(codigoProfessor)
         }
     }
 }
